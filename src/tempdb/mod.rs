918067fd67b0518ea2ae80e3978368df5852ab9e
C.Solovev <constantine.solovev@gmail.com>
2018-10-22 15:59:25 +0400

    Add test for select

--- src/tempdb/mod.rs
+++ src/tempdb/mod.rs
@@ -700,6 +700,73 @@ mod test {
         result
     }
 
+    fn row_to_string(row: Box<[Variant]>) -> String {
+        row.iter()
+            .map(|elem| elem.to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    }
+
+    fn rows_to_strings<'a>(rows: Box<Iterator<Item = Box<[Variant]>> + 'a>) -> Vec<String> {
+        rows.into_iter().map(row_to_string).collect()
+    }
+
+    #[test]
+    fn select_test() {
+        let db = &mut TempDb::new();
+
+        create_table(db, "Users").unwrap();
+        fill_table(db, "Users").unwrap();
+
+        match db.do_query("select * from Users;").unwrap() {
+            ExecuteStatementResponse::Select { column_names, rows } => {
+                assert_eq!(column_names.to_vec(), vec!["id", "name", "age"]);
+                assert_eq!(
+                    rows_to_strings(rows),
+                    vec!["1, Isaac Asimov, 50", "2, Stanislaw Lem, 40", "3, Liu Cixin, 30"]
+                );
+            },
+            _ => panic!("Expected Select result"),
+        };
+
+        match db.do_query("select min(id) as min, name from Users where age >= 30 and name <> 'Roger Zelazny'").unwrap() {
+            ExecuteStatementResponse::Select { column_names, rows } => {
+                assert_eq!(column_names.to_vec(), vec!["min", "name"]);
+                assert_eq!(
+                    rows_to_strings(rows),
+                    vec!["1, Isaac Asimov"]
+                );
+            },
+            _ => panic!("Expected Select result"),
+        };
+
+        match db.do_query("select avg(age) as avg, name from Users group by age having id > 2").unwrap() {
+            ExecuteStatementResponse::Select { column_names, rows } => {
+                assert_eq!(column_names.to_vec(), vec!["avg", "name"]);
+                assert_eq!(
+                    rows_to_strings(rows),
+                    vec!["30, Liu Cixin"]
+                );
+            },
+            _ => panic!("Expected Select result"),
+        };
+
+        match db.do_query("select min(age) as min, max(age) as max, count(age) as count, sum(age) as \
+            sum, avg(age) as avg FROM Users").unwrap() {
+            ExecuteStatementResponse::Select { column_names, rows } => {
+                assert_eq!(column_names.to_vec(), vec!["min", "max", "count", "sum", "avg"]);
+                assert_eq!(
+                    rows_to_strings(rows),
+                    vec!["30, 50, 3, 120, 40"]
+                );
+            },
+            _ => panic!("Expected Select result"),
+        };
+
+        // todo add more test cases, including join, sub queries and so on
+
+    }
+
     #[test]
     fn delete_test() {
         let db = &mut TempDb::new();
